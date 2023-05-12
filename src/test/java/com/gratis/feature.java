70911071f56ebfc445570b668bee0751a18bed2e
AnilAydemir <cazorte@gmail.com>
2023-05-12 15:33:20 +0300

    First commit

---
+++ src/test/java/com/gratis/feature.java
@@ -0,0 +1,41 @@
+package com.gratis;
+
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+
+public class feature {
+
+    @Test
+    public void e2eTest(){
+
+
+       // goToMainPage();
+
+       // validateTitle("Beymen.com – Türkiye’nin Tek Dijital Lüks Platformu");
+
+       // mouseHover("cilt bakımı");
+
+       //  clickSubContentRandom();
+
+        // validateUrlContain();
+
+        // chooseCheckBoxes();
+
+        // validateCheckBoxes();
+
+        // chooseOneProduct();
+
+        // textToExcelColumn("ürün bilgisi", writeExcelFile()[0]);
+
+        // textToExcelColumn("ürün tutarı", writeExcelFile()[1]);
+
+        // addToBasket();
+
+        // validateLoginPage();
+
+        // textToUsername(readExcelFile()[0])
+
+        // textToPassword(readExcelFile()[1])
+
+    }
+}
